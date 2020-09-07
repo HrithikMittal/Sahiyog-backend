312af2e91fbb4a17d460f24b40e5b75a7e96ee70
Adhikansh Mittal <adhikanshmittal@gmail.com>
2020-09-07 22:10:51 +0530

    create User and Order models

---
+++ modals/Order.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var { ObjectId } = mongoose.Schema;
+
+var orderSchema = mongoose.Schema({
+  user: {
+    type: ObjectId,
+    ref: "User",
+  },
+  labPartner: {
+    type: ObjectId,
+    ref: "LabPartner",
+  },
+  test: {
+    type: ObjectId,
+    ref: "Test",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+  paymentMode: {
+    type: Boolean, // 0 -> offline and 1 -> online
+    default: 0,
+  },
+  paymentResponse: [
+    {
+      type: String,
+    },
+  ],
+});
+
+module.exports = Order = mongoose.model("Order", orderSchema);
