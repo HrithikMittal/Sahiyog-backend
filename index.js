e1aaa29b751d122d36f1d4dddfba201a5db099a6
HrithikMittal <adhikanshmittal@gmail.com>
2020-09-10 23:34:22 +0530

    try with heroku

--- index.js
+++ index.js
@@ -26,18 +26,18 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// const userRoute = require("./routes/User");
-// const adminRoute = require("./routes/Admin");
-const medicineRoute = require("./routes/Medicine");
-// const labPartnerRoute = require("./routes/LabPartner");
+const userRoute = require("./routes/User");
+const adminRoute = require("./routes/Admin");
+// const medicineRoute = require("./routes/Medicine");
+const labPartnerRoute = require("./routes/LabPartner");
 // const productRoute = require("./routes/Product");
 // const testRoute = require("./routes/Test");
 // const packageRoute = require("./routes/Package");
 
-// app.use("/user", userRoute);
-// app.use("/admin", adminRoute);
-app.use("/medicine", medicineRoute);
-// app.use("/labpartner", labPartnerRoute);
+app.use("/user", userRoute);
+app.use("/admin", adminRoute);
+// app.use("/medicine", medicineRoute);
+app.use("/labpartner", labPartnerRoute);
 // app.use("/product", productRoute);
 // app.use("/test", testRoute);
 // app.use("/package", packageRoute);
