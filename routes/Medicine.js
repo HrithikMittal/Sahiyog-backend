6dec3253667b958ed3b2536f1a7df214edd66f69
HrithikMittal <adhikanshmittal@gmail.com>
2020-09-10 23:45:14 +0530

    push it

--- routes/Medicine.js
+++ routes/Medicine.js
@@ -5,10 +5,10 @@ const medicineController = require("../controllers/Medicine");
 
 router.post("/createMedicine", medicineController.createMedicine);
 router.put("/updateMedicine/:medicineId", medicineController.updateMedicine);
-router.delete("/deleteMedicine/:medicineId", medicineController.deleteMedicine);
+// router.delete("/deleteMedicine/:medicineId", medicineController.deleteMedicine);
 
-router.get("/getAllMedicines", medicineController.getAllMedicines);
-router.get("/getMedicine/:medicineId", medicineController.getMedicine);
+// router.get("/getAllMedicines", medicineController.getAllMedicines);
+// router.get("/getMedicine/:medicineId", medicineController.getMedicine);
 
 router.param("medicineId", medicineController.medicineById);
 
