dfffc684402589daead053d6df1082172f8a66fe
HrithikMittal <adhikanshmittal@gmail.com>
2020-09-11 00:41:30 +0530

    yess

--- routes/Medicine.js
+++ routes/Medicine.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const medicineController = require("../controllers/Medicine");
 
-// router.post("/createMedicine", medicineController.createMedicine);
-// router.put("/updateMedicine/:medicineId", medicineController.updateMedicine);
-// router.delete("/deleteMedicine/:medicineId", medicineController.deleteMedicine);
+router.post("/createMedicine", medicineController.createMedicine);
+router.put("/updateMedicine/:medicineId", medicineController.updateMedicine);
+router.delete("/deleteMedicine/:medicineId", medicineController.deleteMedicine);
 
-// router.get("/getAllMedicines", medicineController.getAllMedicines);
-// router.get("/getMedicine/:medicineId", medicineController.getMedicine);
+router.get("/getAllMedicines", medicineController.getAllMedicines);
+router.get("/getMedicine/:medicineId", medicineController.getMedicine);
 
-// router.param("medicineId", medicineController.medicineById);
+router.param("medicineId", medicineController.medicineById);
 
 module.exports = router;
