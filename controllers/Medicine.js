2333a31d9c37696665e2a63d7d1dad3579128eca
HrithikMittal <adhikanshmittal@gmail.com>
2020-09-10 23:46:59 +0530

    still bbug

--- controllers/Medicine.js
+++ controllers/Medicine.js
@@ -27,61 +27,61 @@ const createMedicine = (req, res) => {
     });
 };
 
-const updateMedicine = (req, res) => {
-  let medicine = req.medicine;
-  medicine = _.extend(medicine, req.body);
-  medicine
-    .save()
-    .then((result) => {
-      res.json({ message: "Successfully updated!", result });
-    })
-    .catch((err) => {
-      console.log("ERROR in updating medicine controller", err);
-      return res.json({ error: err });
-    });
-};
+// const updateMedicine = (req, res) => {
+//   let medicine = req.medicine;
+//   medicine = _.extend(medicine, req.body);
+//   medicine
+//     .save()
+//     .then((result) => {
+//       res.json({ message: "Successfully updated!", result });
+//     })
+//     .catch((err) => {
+//       console.log("ERROR in updating medicine controller", err);
+//       return res.json({ error: err });
+//     });
+// };
 
-const deleteMedicine = (req, res) => {
-  Medicine.deleteOne({ _id: req.medicine._id })
-    .then((result) => {
-      res.json({ message: "Medicine deleted successfully!", result });
-    })
-    .catch((err) => {
-      console.log("ERRRor in deleting the medicine controller", err);
-      res.json({ error: err });
-    });
-};
+// const deleteMedicine = (req, res) => {
+//   Medicine.deleteOne({ _id: req.medicine._id })
+//     .then((result) => {
+//       res.json({ message: "Medicine deleted successfully!", result });
+//     })
+//     .catch((err) => {
+//       console.log("ERRRor in deleting the medicine controller", err);
+//       res.json({ error: err });
+//     });
+// };
 
-const medicineById = (req, res, next, id) => {
-  Medicine.findById(id).exec((err, medicine) => {
-    if (!medicine || err) {
-      return res.json({ error: "Medicine Not Found!" });
-    }
-    req.medicine = medicine;
-    next();
-  });
-};
+// const medicineById = (req, res, next, id) => {
+//   Medicine.findById(id).exec((err, medicine) => {
+//     if (!medicine || err) {
+//       return res.json({ error: "Medicine Not Found!" });
+//     }
+//     req.medicine = medicine;
+//     next();
+//   });
+// };
 
-const getAllMedicines = (req, res) => {
-  Medicine.find()
-    .then((medicine) => {
-      return res.json({ medicine });
-    })
-    .catch((err) => {
-      console.log("ERROr in getting all medicine controller", err);
-      res.json({ error: err });
-    });
-};
+// const getAllMedicines = (req, res) => {
+//   Medicine.find()
+//     .then((medicine) => {
+//       return res.json({ medicine });
+//     })
+//     .catch((err) => {
+//       console.log("ERROr in getting all medicine controller", err);
+//       res.json({ error: err });
+//     });
+// };
 
-const getMedicine = (req, res) => {
-  return res.json(req.medicine);
-};
+// const getMedicine = (req, res) => {
+//   return res.json(req.medicine);
+// };
 
 module.exports = {
   createMedicine,
-  updateMedicine,
-  deleteMedicine,
-  medicineById,
-  getAllMedicines,
-  getMedicine,
+  // updateMedicine,
+  // deleteMedicine,
+  // medicineById,
+  // getAllMedicines,
+  // getMedicine,
 };
--- routes/Medicine.js
+++ routes/Medicine.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const medicineController = require("../controllers/Medicine");
 
 router.post("/createMedicine", medicineController.createMedicine);
-router.put("/updateMedicine/:medicineId", medicineController.updateMedicine);
+// router.put("/updateMedicine/:medicineId", medicineController.updateMedicine);
 // router.delete("/deleteMedicine/:medicineId", medicineController.deleteMedicine);
 
 // router.get("/getAllMedicines", medicineController.getAllMedicines);
 // router.get("/getMedicine/:medicineId", medicineController.getMedicine);
 
-router.param("medicineId", medicineController.medicineById);
+// router.param("medicineId", medicineController.medicineById);
 
 module.exports = router;
