7ce39909d273bab7fbd6ed7efc163a56b43d2e47
HrithikMittal <adhikanshmittal@gmail.com>
2020-09-10 23:25:33 +0530

    try with heroku

--- index.js
+++ index.js
@@ -26,21 +26,21 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const userRoute = require("./routes/User");
-const adminRoute = require("./routes/Admin");
-const medicineRoute = require("./routes/Medicine");
-const labPartnerRoute = require("./routes/LabPartner");
-const productRoute = require("./routes/Product");
-const testRoute = require("./routes/Test");
-const packageRoute = require("./routes/Package");
+// const userRoute = require("./routes/User");
+// const adminRoute = require("./routes/Admin");
+// const medicineRoute = require("./routes/Medicine");
+// const labPartnerRoute = require("./routes/LabPartner");
+// const productRoute = require("./routes/Product");
+// const testRoute = require("./routes/Test");
+// const packageRoute = require("./routes/Package");
 
-app.use("/user", userRoute);
-app.use("/admin", adminRoute);
-app.use("/medicine", medicineRoute);
-app.use("/labpartner", labPartnerRoute);
-app.use("/product", productRoute);
-app.use("/test", testRoute);
-app.use("/package", packageRoute);
+// app.use("/user", userRoute);
+// app.use("/admin", adminRoute);
+// app.use("/medicine", medicineRoute);
+// app.use("/labpartner", labPartnerRoute);
+// app.use("/product", productRoute);
+// app.use("/test", testRoute);
+// app.use("/package", packageRoute);
 
 var data = require("./docs/home.json");
 app.get("/", (req, res) => {
